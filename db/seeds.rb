d7be39139d51ca746b40d96942a3da9ad6b3e29e
Franz Schurmann <schuermann.franz@gmail.com>
2023-11-21 16:40:47 +0100

    Added Booking Model, Controller and View

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Robot.create(overview: "Super gud robto", model: "Atlas", price: "120.00", rating: "5.0", image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wired.com%2Fstory%2Fatlas-robot-does-backflips-now%2F&psig=AOvVaw3ulK1JOYRcDOotSK4ezFaT&ust=1700662254267000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCSisGi1YIDFQAAAAAdAAAAABAI", user_id: "1")
