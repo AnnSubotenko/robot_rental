55401c39e464a60e3154173f50c4747ffb9a6519
Giorgio Mazzuca <mazzucagiorgio@gmail.com>
2023-11-23 17:47:47 +0100

    modal popUp created when the user successfully booked the robot

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :robot
+  belongs_to :user
 
 end
--- db/migrate/20231121145921_create_bookings.rb
+++ db/migrate/20231121145921_create_bookings.rb
@@ -2,6 +2,7 @@ class CreateBookings < ActiveRecord::Migration[7.1]
   def change
     create_table :bookings do |t|
       t.references :robot, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
