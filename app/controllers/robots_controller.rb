241f3195cb5712bc6bd26666ebee23f9ee766a4f
BERNARDO LISI <bernardo.lisi.work@gmail.com>
2023-11-24 11:30:39 +0100

    robot controller new&create action

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -7,4 +7,25 @@ class RobotsController < ApplicationController
     @robot = Robot.find(params[:id])
     @booking = Booking.find_by(robot: @robot)
   end
+
+  def new
+    @robot = Robot.new
+  end
+
+  def create
+    @robot = Robot.new(robot_params)
+    @user = current_user
+    @robot.user_id = @user.id
+    if @robot.save
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def robot_params
+    params.require(:robot).permit(:model, :overview, :price)
+  end
 end
